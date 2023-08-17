30be4212ecd06571e69d59f6191e75b0fb26b3bb
Kevin Kor <kevin.h.kor@gmail.com>
2023-08-17 15:53:08 -0700

    Parameterize query and create class for transform.

--- dags/etl/la_crime_transform.py
+++ dags/etl/la_crime_transform.py
@@ -2,39 +2,39 @@ import psycopg2
 from datetime import date, timedelta
 from config.config import Config as cfg
 
+class LA_Crime_Transfom:
 
-def transform_data():
-    '''
-    Calls the stored procedure on PostgreSQL db to transform the data.
-    '''
+    def __init__(self):
+         # Connect to the PostgreSQL database
+        self.connection = psycopg2.connect(
+            host=cfg.host,
+            port=cfg.port,
+            database=cfg.database,
+            user=cfg.user,
+            password=cfg.password
+        )
 
-    # Connect to the PostgreSQL database
-    connection = psycopg2.connect(
-        host=cfg.host,
-        port=cfg.port,
-        database=cfg.database,
-        user=cfg.user,
-        password=cfg.password
-    )
-
-    cursor = connection.cursor()
-    cursor.execute("SELECT COUNT(*) FROM {table}".format(table='final.crime_logs'))
-    # Fetch the result of the query
-    count = cursor.fetchone()[0]
-    if count==0:
-        processed_date = ''
-    else:
-        processed_date = str(date.today() - timedelta(days=3))
-
-    try:
-        cursor = connection.cursor()
-        if processed_date:
-            cursor.execute("CALL final.sp_transform_crime_logs('{date}'::date)".format(date=processed_date))
+    def transform_data(self):
+        '''
+        Calls the stored procedure on PostgreSQL db to transform the data.
+        '''
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT COUNT(*) FROM {table}".format(table='final.crime_logs'))
+        # Fetch the result of the query
+        count = cursor.fetchone()[0]
+        if count==0:
+            processed_date = ''
         else:
-            cursor.execute("CALL final.sp_transform_crime_logs(NULL)")  # Pass NULL when the parameter is empty
-        connection.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        connection.rollback()
-    finally:
-        cursor.close()
+            processed_date = str(date.today() - timedelta(days=3))
+
+        try:
+            if processed_date:
+                cursor.execute("CALL final.sp_transform_crime_logs(%s::date)", (processed_date,))
+            else:
+                cursor.execute("CALL final.sp_transform_crime_logs(NULL)")  # Pass NULL when the parameter is empty
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error: %s" % error)
+            self.connection.rollback()
+        finally:
+            cursor.close()
